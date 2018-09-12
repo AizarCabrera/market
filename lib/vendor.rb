4054d1b0b4b1377a60d052be9a3ee94680a0fea2
Aizar <aizar.cabrera@icloud.com>
2018-09-12 13:16:30 -0600

    add test class for vendor and test

---
+++ lib/vendor.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+class Vendor
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
---
+++ test/vendor_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists
+  vendor = Vendor.new("Rocky Mountain Fresh")
+  assert_instance_of Vendor, vendor
+  end
+
+  def test_it_exists
+  vendor = Vendor.new("Rocky Mountain Fresh")
+  assert_equal "Rocky Mountain Fresh", vendor.name
+  end
+end
